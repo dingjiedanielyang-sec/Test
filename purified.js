8eea4d4b7cce01a4848ed2b46d73f63b59c0ea27
DingjieDanielYang <48067965+dingjiedanielyang-sec@users.noreply.github.com>
2022-05-16 12:06:48 -0700

    Update purified.js

--- purified.js
+++ purified.js
@@ -1,7 +1,12 @@
 import { seal } from './utils.js';
 
-// eslint-disable-next-line unicorn/better-regex
+
 var t = /<script type="application\/json">((.|\s)*?)<\/script>/;
+
+if(host && host.toLowerCase().includes('mydomain.com')){
+  // Do something 
+}
+
 export const isDevEnv = () => {
   return apiHost() && apiHost().toLowerCase().includes('mydev.com');}
 var EMAIL_REGEX = /\A(([a-z0-9.!\#$%&'*+\/=?^_`{|}~-]+)@((?:[-a-z0-9]+\.)+[a-z]{2,}))(\s*,\s*([^:@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,}))*\z/i
